60bd1e7fc70bd539a595ade034dfb321f07fc908
伯方シオ <cuin@hotmail.co.jp>
2017-12-31 09:08:11 -1000

    status取る装置
    デバック用

---
+++ get_status.py
@@ -0,0 +1,24 @@
+from mastodon import *
+import warnings, traceback
+
+#Winのプロンプトから起動するならこれ追加ね↓
+"""
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,
+                              encoding=sys.stdout.encoding,
+                              errors='backslashreplace',
+                              line_buffering=sys.stdout.line_buffering)
+                              """
+warnings.simplefilter("ignore", UnicodeWarning)
+
+"""ログイントークン取得済みで動かしてね（*'∀'人）"""
+
+url_ins = open("instance.txt").read()
+
+mastodon = Mastodon(
+    client_id="cred.txt",
+    access_token="auth.txt",
+    api_base_url=url_ins)  # インスタンス
+
+ids = input(">> ")
+status = mastodon.status(ids)
+print(status)
